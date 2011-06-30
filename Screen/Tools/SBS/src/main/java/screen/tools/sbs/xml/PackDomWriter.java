dab34c2ce2b6a735fff5e9e325ffac1803cd3e03
thoratou <thoratou@470984fe-aa8c-d0eb-2131-b18db9c326a8>
2011-06-30 20:27:42 +0000

    dependency part into PackDomWriter implemented (couldn't be tested for now)
    git-svn-id: http://sbs.googlecode.com/svn/trunk@105 470984fe-aa8c-d0eb-2131-b18db9c326a8

--- Screen/Tools/SBS/src/main/java/screen/tools/sbs/xml/PackDomWriter.java
+++ Screen/Tools/SBS/src/main/java/screen/tools/sbs/xml/PackDomWriter.java
@@ -25,6 +25,8 @@ package screen.tools.sbs.xml;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 import org.jdom.Document;
 import org.jdom.Element;
@@ -32,9 +34,15 @@ import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
 import screen.tools.sbs.context.ContextHandler;
+import screen.tools.sbs.objects.Dependency;
+import screen.tools.sbs.objects.Description;
+import screen.tools.sbs.objects.Flag;
+import screen.tools.sbs.objects.Library;
 import screen.tools.sbs.objects.ProjectProperties;
 import screen.tools.sbs.objects.TinyPack;
+import screen.tools.sbs.utils.FieldBuildMode;
 import screen.tools.sbs.utils.FieldException;
+import screen.tools.sbs.utils.FieldPath;
 import screen.tools.sbs.utils.FieldString;
 
 public class PackDomWriter {
@@ -51,6 +59,18 @@ public class PackDomWriter {
 		
 		ProjectProperties properties = pack.getProperties();
 		writeProperties(properties,root);
+		
+		List<Dependency> dependencyList = pack.getDependencyList();
+		writeDependencies(dependencyList,root);
+		
+		List<Description> descriptionList = pack.getDescriptionList();
+		writeDescriptions(descriptionList,root);
+		
+		List<Flag> flagList = pack.getFlagList();
+		writeFlags(flagList,root);
+		
+		//List<Import> importList = pack.getImportList();
+		//writeImports(importList,root);
 		
 		writeDom(document,path,file);
 	}
@@ -75,6 +95,103 @@ public class PackDomWriter {
 		propertiesElt.addContent(buildTypeElt);
 	
 		root.addContent(propertiesElt);
+	}
+
+	private void writeDependencies(List<Dependency> dependencyList, Element root) throws FieldException {
+		if(!dependencyList.isEmpty()){
+			Element dependencyElt = new Element("dependencies");
+
+			Iterator<Dependency> iterator = dependencyList.iterator();
+			while(iterator.hasNext()){
+				Dependency next = iterator.next();
+				writeDependency(next, dependencyElt);
+			}
+			
+			root.addContent(dependencyElt);
+		}
+	}
+	
+	private void writeDependency(Dependency dependency, Element dependencyRoot) throws FieldException {
+		Element dependencyElt = new Element("dependency");
+		dependencyElt.setAttribute("name", dependency.getName().getString());
+		dependencyElt.setAttribute("version", dependency.getVersion().getString());
+		if(dependency.getExport().getBool())
+			dependencyElt.setAttribute("export", "true");
+		
+		List<FieldPath> includePathList = dependency.getIncludePathList();
+		writeIncludePathList(includePathList,dependencyElt);
+		
+		List<Library> libraryList = dependency.getLibraryList();
+		List<FieldPath> libraryPathList = dependency.getLibraryPathList();
+		
+		if(!libraryList.isEmpty() || !libraryPathList.isEmpty()){
+			Element libraryRoot = new Element("libraries");
+			writeLibraryList(libraryList,libraryRoot);
+			writeLibraryPathList(libraryPathList,libraryRoot);
+			dependencyElt.addContent(libraryRoot);
+		}
+		
+		dependencyRoot.addContent(dependencyElt);
+	}
+
+	private void writeIncludePathList(List<FieldPath> includePathList,
+			Element dependencyElt) throws FieldException {
+		if(!includePathList.isEmpty()){
+			Element includePathListEtl = new Element("includes");
+			
+			Iterator<FieldPath> iterator = includePathList.iterator();
+			while(iterator.hasNext()){
+				FieldPath next = iterator.next();
+				writePath(next,includePathListEtl);
+			}
+			
+			dependencyElt.addContent(includePathListEtl);
+		}
+	}
+
+	private void writeLibraryPathList(List<FieldPath> libraryPathList,
+			Element libraryRoot) throws FieldException {
+		Iterator<FieldPath> iterator = libraryPathList.iterator();
+		while(iterator.hasNext()){
+			FieldPath next = iterator.next();
+			writePath(next, libraryRoot);
+		}
+	}
+
+	private void writeLibraryList(List<Library> libraryList, Element libraryRoot) throws FieldException {
+		Iterator<Library> iterator = libraryList.iterator();
+		while(iterator.hasNext()){
+			Library next = iterator.next();
+			writeLibrary(next,libraryRoot);
+		}
+	}
+
+	private void writeLibrary(Library library, Element libraryRoot) throws FieldException {
+		Element libraryElt = new Element("lib");
+		libraryElt.setText(library.getName().getString());
+		libraryRoot.addContent(libraryElt);
+	}
+
+	private void writePath(FieldPath fieldPath, Element pathRoot) throws FieldException {
+		Element pathElt = new Element("path");
+		
+		FieldBuildMode buildMode = fieldPath.getBuildMode();
+		if(buildMode.get() != FieldBuildMode.Type.ALL)
+			pathElt.setAttribute("build", buildMode.getAsString());
+		pathElt.setText(fieldPath.getString());
+		
+		pathRoot.addContent(pathElt);
+	}
+	
+	private void writeFlags(List<Flag> flagList, Element root) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void writeDescriptions(List<Description> descriptionList,
+			Element root) {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private void writeDom(Document document, String path, String file) {
